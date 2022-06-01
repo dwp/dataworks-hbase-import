d4708e29a8897d24c20ffa731c73fb6165cc8fc6
rorybryett <rory.bryett@dwp.gov.uk>
2022-06-01 22:38:10 +0100

    script corections

--- files/hbase-snapshot-importer.sh.tpl
+++ files/hbase-snapshot-importer.sh.tpl
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 SNAPSHOT_NAME=$1
-SNAPSHOT_SOURCE_LOCATION=$2
+SNAPSHOT_SOURCE_BUCKET=$2
+SNAPSHOT_SOURCE_PREFIX=$3
 
 HBASE_ROOT_BUCKET=${hbase_root_bucket}
 HBASE_ROOT_DIR=${hbase_root_dir}
 
 
-echo "importing snapshot $${SNAPSHOT_NAME} from $${SNAPSHOT_SOURCE_LOCATION}"
-hbase snapshot export -snapshot $${SNAPSHOT_NAME} -copy-from s3a://$${SNAPSHOT_SOURCE_LOCATION} -copy-to s3a://$${HBASE_ROOT_BUCKET}/$${HBASE_ROOT_DIR}
+echo "importing snapshot '$${SNAPSHOT_NAME}' from 's3://$${SNAPSHOT_SOURCE_BUCKET}/$${SNAPSHOT_SOURCE_PREFIX}'"
+hbase snapshot export -snapshot $${SNAPSHOT_NAME} -copy-from s3://$${SNAPSHOT_SOURCE_BUCKET}/$${SNAPSHOT_SOURCE_PREFIX} -copy-to s3://$${HBASE_ROOT_BUCKET}/$${HBASE_ROOT_DIR}
 if [ $? != 0 ]; then
     echo "import failed. exiting..."
     exit 1
 else
     echo "import finished"
 fi
-
--- files/hbase-snapshot-restorer.sh
+++ files/hbase-snapshot-restorer.sh
@@ -1,4 +1,89 @@
 #!/bin/bash
 
-SNAPSHOT_NAME=$1
-SNATSHOT_SOURCE_LOCATION=$2
+COMMAND=$1
+SNAPSHOT_NAME=$2
+TABLE_NAME=$3
+
+function clone_snapshot() {
+    if [ -z "${TABLE_NAME}" ]; then
+        echo "'clone' command expects 'TABLE_NAME' argument, exiting..."
+        exit 1
+    fi
+
+    if check_if_table_exists ${TABLE_NAME}; then
+        echo "cannot clone snapshot '${SNAPSHOT_NAME}' as table with name '${TABLE_NAME}' already exists, exiting..."
+        exit 1
+    fi
+
+    echo "cloning snapshot '${SNAPSHOT_NAME}' into table '${TABLE_NAME}"
+    echo "clone_snapshot '${SNAPSHOT_NAME}', '${TABLE_NAME}'" | hbase shell -n
+    if [ $? != 0 ]; then
+        echo "restore failed. exiting..."
+        exit 1
+    else
+        echo "restore finished"
+    fi
+}
+
+function restore_snapshot() {    
+    table=$(get_table_name_from_snapshot)
+
+    if check_if_table_exists $table; then
+        disable_table ${table}
+    fi
+
+    echo "restoring snapshot '${SNAPSHOT_NAME}'"
+    echo "restore_snapshot '${SNAPSHOT_NAME}'" | hbase shell -n
+    if [ $? != 0 ]; then
+        echo "restore failed. exiting..."
+        exit 1
+    else
+        echo "restore finished"
+    fi
+}
+
+function check_if_table_exists() {
+    local table_name=$1
+
+    echo "checking if table '${table_name}' exists"
+    if echo -e "exists '${table_name}'" | hbase shell 2>&1 | grep -q "does exist" 2>/dev/null 
+    then
+        echo "Table '${table_name}' exists"
+        return 0
+    else
+        echo "Table '${table_name}' does not exist"
+        return 1
+    fi
+}
+
+function disable_table() {
+    local table=$1
+
+    echo "disabling table '${table}'"
+    echo "disable '${table}'" | hbase shell -n
+    if [ $? != 0 ]; then
+        echo "disable table failed. exiting..."
+        exit 1
+    else
+        echo "disable table finished"
+    fi
+}
+
+function get_table_name_from_snapshot() {
+    local table_name=$( hbase snapshot info -snapshot "${SNAPSHOT_NAME}" | sed -n -e 's/^.*Table: //p' )
+    echo $table_name
+}
+
+case $COMMAND in
+    clone)
+        clone_snapshot
+        ;;
+
+    restore)
+        restore_snapshot
+        ;;
+
+    *)
+        echo "unknown command"
+        ;;
+esac
