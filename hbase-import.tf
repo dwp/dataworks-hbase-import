f0c645025b53d7819b556b6ee236e1c8c0e19cba
rorybryett <rory.bryett@dwp.gov.uk>
2022-06-16 13:44:10 +0100

    DW-8766 extend the S3 bucket policy of the target source export bucket to allow for retrieval of the Snapshot object from across AWS Accounts

--- hbase-import.tf
+++ hbase-import.tf
@@ -26,28 +26,3 @@ resource "aws_s3_bucket_object" "hbase_snapshot_restorer_script" {
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
-data "aws_iam_policy_document" "hbase_s3_import" {
-  statement {
-    sid    = "GetSnapshotFromExportBucket"
-    effect = "Allow"
-
-    actions = [
-      "kms:Decrypt"
-    ]
-
-    resources = [
-      data.terraform_remote_state.hbase_export.outputs.hbase_export_bucket.key_arn,
-    ]
-  }
-}
-
-resource "aws_iam_policy" "hbase_s3_import" {
-  name        = "IngestHbaseS3Import"
-  description = "Allow Ingestion EMR cluster to import HBASE Snapshots from the export bucket"
-  policy      = data.aws_iam_policy_document.hbase_s3_import.json
-}
-
-resource "aws_iam_role_policy_attachment" "emr_ingest_hbase_main" {
-  role       = data.terraform_remote_state.internal_compute.outputs.emr_instance_role.id
-  policy_arn = aws_iam_policy.hbase_s3_import.arn
-}
--- locals.tf
+++ locals.tf
@@ -3,4 +3,13 @@ locals {
     snapshot_source_prefix = "snapshots"
     hbase_root_bucket = data.terraform_remote_state.internal_compute.outputs.aws_emr_cluster.root_bucket
     hbase_root_dir = data.terraform_remote_state.internal_compute.outputs.aws_emr_cluster.root_directory
+
+    source_hbase_snapshot_environment = var.source_hbase_snapshot_workspace == "default" ? "development" : var.source_hbase_snapshot_workspace
+    accounts = {
+        development    = "475593055014"
+        qa             = "003710426629"
+        integration    = "337387380515"
+        preprod        = "445364893246"
+        production     = "517406633788"
+    }
 }
