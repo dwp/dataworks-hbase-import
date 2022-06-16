234c20e092c4596141df19f9d2056d32a9e350d9
rorybryett <rory.bryett@dwp.gov.uk>
2022-06-16 15:18:40 +0100

    bucket policy goes in export repo

--- cross-account-access.tf
+++ cross-account-access.tf
@@ -23,30 +23,3 @@ resource "aws_iam_role_policy_attachment" "emr_ingest_hbase_main" {
   role       = data.terraform_remote_state.internal_compute.outputs.emr_instance_role.id
   policy_arn = aws_iam_policy.hbase_s3_import.arn
 }
-
-data "aws_iam_policy_document" "ingest_emr_access_to_hbase_export_bucket" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [data.terraform_remote_state.internal_compute.outputs.emr_instance_role.arn]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      data.terraform_remote_state.hbase_export.outputs.hbase_export_bucket.arn,
-      "${data.terraform_remote_state.hbase_export.outputs.hbase_export_bucket.arn}/*",
-    ]
-  }
-}
-
-resource "aws_s3_bucket_policy" "ingest_emr_access_to_hbase_export_bucket" {
-  provider = aws.hbase_snapshot_source
-
-  bucket = data.terraform_remote_state.hbase_export.outputs.hbase_export_bucket.id
-  policy = data.aws_iam_policy_document.ingest_emr_access_to_hbase_export_bucket.json
-}
-
